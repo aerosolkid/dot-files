95e16917d4873a373b8824efa060ed5d2216550b
Mark Clements <mark@mpc.io>
2015-01-20 16:55:19 -0500

    tidying up

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
-ln -s ~/Dropbox/dotfiles/.aspell.en.prepl ~/.aspell.en.prepl
-ln -s ~/Dropbox/dotfiles/.aspell.en.pws   ~/.aspell.en.pws
-ln -s ~/Dropbox/dotfiles/.bash_profile    ~/.bash_profile
-ln -s ~/Dropbox/dotfiles/.bashrc          ~/.bashrc
-ln -s ~/Dropbox/dotfiles/.hgrc            ~/.hgrc
-ln -s ~/Dropbox/dotfiles/.hgignore        ~/.hgignore
+#!/bin/bash
+
+#
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+dotfile_dir=`pwd`
+
+rm -f ~/.aspell.en.prepl
+rm -f ~/.aspell.en.pws
+rm -f ~/.bash_profile
+rm -f ~/.bashrc
+rm -f ~/.hgrc
+rm -f ~/.hgignore
+rm -f ~/.gitconfig
+rm -f ~/.gitignore_global
+
+ln  -s ${dotfile_dir}/.aspell.en.prepl ~/.aspell.en.prepl
+ln  -s ${dotfile_dir}/.aspell.en.pws   ~/.aspell.en.pws
+ln  -s ${dotfile_dir}/.bash_profile    ~/.bash_profile
+ln  -s ${dotfile_dir}/.bashrc          ~/.bashrc
+ln  -s ${dotfile_dir}/.hgrc            ~/.hgrc
+ln  -s ${dotfile_dir}/.hgignore        ~/.hgignore
+ln  -s ${dotfile_dir}/.gitconfig       ~/.gitconfig 
+ln  -s ${dotfile_dir}/.gitignore       ~/.gitignore
