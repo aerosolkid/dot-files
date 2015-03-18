6e360f781ae2539aad3fa0f707898b8f6151e61a
Mark Clements <mark@mpc.io>
2015-03-18 14:16:17 -0400

    added brew-cask.sh

---
+++ brew-cask.sh
@@ -0,0 +1,74 @@
+# -*- mode: shell-script -*-
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# QuickLook Plugins
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+brew cask install betterzipql
+brew cask install webpquicklook
+brew cask install suspicious-package
+
+# daily
+#brew cask install alfred
+brew cask install backblaze
+brew cask install bartender
+brew cask install controlplane
+brew cask install default-folder-x
+brew cask install divvy
+brew cask install dropbox
+brew cask install emacs
+brew cask install evernote
+brew cask install flux
+#brew cask install gyazo
+brew cask install macpaw-gemini
+brew cask install marked
+brew cask install onepassword
+#brew cask install rescuetime
+brew cask install textexpander
+brew cask install totalfinder
+brew cask install transporter-desktop
+brew cask install quicksilver
+brew cask install vmware-fusion
+
+# dev
+brew cask install iterm2
+#brew cask install sublime-text
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install kaleidoscope
+brew cask install livereload
+brew cask install sourcetree
+brew cask install xscope
+
+# fun
+#brew cask install limechat
+#brew cask install miro-video-converter
+brew cask install vimediamanager
+
+# browsers
+brew cask install google-chrome-canary
+brew cask install firefox-nightly --force
+brew cask install webkit-nightly --force
+#brew cask install chromium --force
+#brew cask install torbrowser
+
+# less often
+#brew cask install disk-inventory-x
+#brew cask install screenflow4 # 4 specifically not 5.
+brew cask install private-eye
+brew cask install vlc
+#brew cask install keycastr  
+
+
+# Not on cask but I want regardless.
+
+# 3Hub
+# File Multi Tool 5
+# Phosphor
