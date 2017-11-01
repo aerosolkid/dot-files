12ee2a89e8388997e37ade26550a7c3f23a7eba7
mpc <mark@mpc.io>
2017-11-01 13:26:15 -0400

    iterm tab settings now ignore shells within emacs

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -7,6 +7,14 @@ if [ "${EMACS:-f}" = "t" ]; then
     export TERM=xterm
 fi
 
-#export PS1="\u@\h:[\W\$(be_get_branch "$2")]\\$ "
+if [ ${INSIDE_EMACS+x} ];then
+  ITERM_TAB=''
+else
+  ITERM_TAB=']1;\h\007'
+fi
+
+SEP=$'\x2d'
+BULLET=$'\xe2\x96\xb8'
+
 #http://bashrcgenerator.com
-export PS1=$'\n\[[1;37m\]\x2d\[[0m\] \u\[[00;37m\]@]1;\h\007\h \[[1;37m\]\x2d\[[0m\] \[[00;36m\]\w \[[1;35m\]$(__git_ps1 "[%s]")\[[0m\]\n\[[38;5;46m\]\xe2\x96\xb8\[[0m\] \[[1;37m\]\\$\[[0m\] '
+export PS1="$ITERM_TAB\n\[[1;37m\]$SEP\[[0m\] \u\[[00;37m\]@\h \[[1;37m\]$SEP\[[0m\] \[[00;36m\]\w \[[1;35m\]$(__git_ps1 "[%s]")\[[0m\]\n\[[38;5;46m\]$BULLET\[[0m\] \[[1;37m\]\\$\[[0m\] "
