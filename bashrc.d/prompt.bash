dcb4b0f36bc69a86d8369549b12ce1585392a0df
mpc <mark@mpc.io>
2017-11-02 10:03:32 -0400

    Added some new themes, and once again tweaked my prompt

--- bashrc.d/prompt.bash
+++ bashrc.d/prompt.bash
@@ -18,4 +18,4 @@ BULLET=$'\xe2\x96\xb8'
 
 #http://bashrcgenerator.com
 #export PS1="$ITERM_TAB\n\[[38;5;46m\]$BULLET\[[0m\] \u\[[00;37m\]@\h  \[[00;36m\]\w \[[1;35m\]$(__git_ps1 "[%s]")\[[0m\]\n\[[1;37m\]\\$\[[0m\] "
-PROMPT_COMMAND='__git_ps1 "$ITERM_TAB\n\[[38;5;46m\]$BULLET\[[32m\] \u\[[1;37m\]@\[[00;30m\]\h  \[[00;36m\]\w\[[1;35m\]" "\[[0m\]\n\[[1;37m\]\\\$\[[0m\] "'
+PROMPT_COMMAND='__git_ps1 "$ITERM_TAB\n\[[0;32m\]$BULLET\[[1;31m\] \u\[[00;30m\]@\[[00;30m\]\h  \[[1;36m\]\w\[[1;35m\]" "\[[0m\]\n\[[1;34m\]\\\$\[[0m\] "'
