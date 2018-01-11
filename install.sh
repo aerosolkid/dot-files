09528012fb7089d7e83435b7ebcc6d429bbd50cc
mpc <mark@mpc.io>
2018-01-11 10:58:27 -0500

    Added git-prompt-theme.  Install should be a little smarter now about existing files amd old links

--- install.sh
+++ install.sh
@@ -7,24 +7,44 @@
 # older versions of bash choke on '-o pipefail',
 # and we're not using pipes
 #set -euo pipefail
-set -eu
+set -u
 
 dotfile_dir=`pwd`
 
 # Create necessary directories
 mkdir -p ~/.ssh
 
+# cleanup old links
+for dfile in ~/.*
+do
+  readlink $dfile | grep -q dotfiles;
+  if [ $? -eq 0 ] # Grep found "dotfiles", so this is our link
+  then
+    rm -f $dfile
+  fi
+done
+
+
 # Link files from dotfiles
-ln -s ${dotfile_dir}/aspell.en.prepl ~/.aspell.en.prepl
-ln -s ${dotfile_dir}/aspell.en.pws   ~/.aspell.en.pws
-ln -s ${dotfile_dir}/bashrc          ~/.bashrc
-ln -s ${dotfile_dir}/bashrc.d        ~/.bashrc.d
-ln -s ${dotfile_dir}/ctags           ~/.ctags
-ln -s ${dotfile_dir}/emacs.d         ~/.emacs.d
-ln -s ${dotfile_dir}/gitconfig       ~/.gitconfig
-ln -s ${dotfile_dir}/gitignore       ~/.gitignore
-ln -s ${dotfile_dir}/hgrc            ~/.hgrc
-ln -s ${dotfile_dir}/hgignore        ~/.hgignore
+for dotfile in aspell.en.prepl aspell.en.pws bashrc bashrc.d ctags emacs.d git-prompt-theme.sh gitconfig gitignore
+do
+  if [ -f ~/.${dotfile} ]
+  then
+    rm -f ~/.${dotfile}
+  fi
+  ln -s ${dotfile_dir}/${dotfile} ~/.${dotfile}
+done
+
+#ln -s ${dotfile_dir}/aspell.en.prepl ~/.aspell.en.prepl
+#ln -s ${dotfile_dir}/aspell.en.pws   ~/.aspell.en.pws
+#ln -s ${dotfile_dir}/bashrc          ~/.bashrc
+#ln -s ${dotfile_dir}/bashrc.d        ~/.bashrc.d
+#ln -s ${dotfile_dir}/ctags           ~/.ctags
+#ln -s ${dotfile_dir}/emacs.d         ~/.emacs.d
+#ln -s ${dotfile_dir}/gitconfig       ~/.gitconfig
+#ln -s ${dotfile_dir}/gitignore       ~/.gitignore
+#ln -s ${dotfile_dir}/hgrc            ~/.hgrc
+#ln -s ${dotfile_dir}/hgignore        ~/.hgignore
 
 # Link .bash_profile -> .bashrc
 rm -f ~/.bash_profile
