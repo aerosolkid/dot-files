1aa4216273eb41fbc2dd44b6f5f259d5d6a789ef
mpc <mark@mpc.io>
2018-01-11 19:28:53 -0500

    Cask makes my head hurt.  I may have to do something about that.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,9 +13,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (pdf-tools yasnippet yard-mode yaml-mode wrap-region wgrep web-mode w3m solarized-theme smex smart-forward scss-mode rust-mode ruby-hash-syntax ruby-end ruby-compilation rubocop rspec-mode rhtml-mode rainbow-mode rainbow-delimiters python-mode projectile-rails php-mode paredit paperless pallet ox-twbs org-bullets muttrc-mode multi-term markdown-mode magit less-css-mode jump js2-mode ido-vertical-mode ido-ubiquitous htmlize header2 haml-mode graphviz-dot-mode gnuplot gitignore-mode gitconfig-mode git-timemachine ghc geiser flycheck-rust flycheck-package flx-ido fill-column-indicator erlang engine-mode dockerfile-mode dired-open dired-details dired+ diminish diff-hl dash-at-point company coffee-mode cm-mode chruby auctex ag)))
+    (async cask dash expand-region f flycheck git-commit haskell-mode inf-ruby magit-popup package-build package-lint projectile s with-editor pdf-tools yasnippet yard-mode yaml-mode wrap-region wgrep web-mode w3m solarized-theme smex smart-forward scss-mode rust-mode ruby-hash-syntax ruby-end ruby-compilation rubocop rspec-mode rhtml-mode rainbow-mode rainbow-delimiters python-mode projectile-rails php-mode paredit paperless pallet ox-twbs org-bullets muttrc-mode multi-term markdown-mode magit less-css-mode jump js2-mode ido-vertical-mode ido-ubiquitous htmlize header2 haml-mode graphviz-dot-mode gnuplot gitignore-mode gitconfig-mode git-timemachine ghc geiser flycheck-rust flycheck-package flx-ido fill-column-indicator erlang engine-mode dockerfile-mode dired-open dired-details dired+ diminish diff-hl dash-at-point company coffee-mode cm-mode chruby auctex ag)))
  '(paperless-capture-directory "/Volumes/Media/File Cabinet/Inbox")
- '(paperless-root-directory "/Volumes/Media/File Cabinet"))
+ '(paperless-root-directory "/Volumes/Media/File Cabinet")
+ '(pdf-tools-handle-upgrades nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
