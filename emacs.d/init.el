d5f03f66ee9f8d2ff12ce66b3a92654e8fa6ad60
mpc <mark@mpc.io>
2018-08-01 09:45:02 -0400

    Emacs configuration changes: added php-mode/autocorrect. web-mode tweaks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,7 +10,7 @@
 ;;  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
 ;;  )
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa")    t)
+(add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/")    t)
 (package-refresh-contents)
 (package-initialize)
 ;;
@@ -28,7 +28,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (multi-term expand-region use-package))))
+ '(package-selected-packages
+   (quote
+    (auto-complete php-mode org multi-term expand-region use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
