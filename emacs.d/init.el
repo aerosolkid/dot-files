d8f834567d1a6395a1725e7a3de511c81fc6c962
mpc <mark@mpc.io>
2019-01-02 13:40:03 -0500

    recent changes

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,18 +1,15 @@
 ;; Package management is first and foremost
 ;;
 (require 'package)
-;;(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-;;                    (not (gnutls-available-p))))
-;;       (proto (if no-ssl "http" "https")))
-;;  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-;;  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-;;  (add-to-list 'package-archives (cons "org"   (concat proto "://orgmode.org/elpa/")) t)
-;;  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-;;  )
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/")    t)
-(package-refresh-contents)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 ;;
 ;; And after that, let's make sure we're not binding META on a mac like an animal
 ;;
@@ -30,7 +27,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (auto-complete php-mode org multi-term expand-region use-package))))
+    (dash-at-point js2-mode js2 auto-complete php-mode org multi-term expand-region use-package)))
+ '(tramp-histfile-override nil nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
